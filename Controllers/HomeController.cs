02c832213513c88768dcfb733cddde37829dde75
nto4 <mehmet94nto@gmail.com>
2019-09-15 00:58:26 +0300

    change course to event

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -9,8 +9,8 @@ namespace CourseAPp.Controllers {
         public IActionResult Index () {
             int saat = DateTime.Now.Hour;
             
-            ViewBag.Greeting = saat > 12 ? "iyi günler" : "günaydın";
-            ViewBag.Username = "Master";
+           // ViewBag.Greeting = saat > 12 ? "iyi günler" : "günaydın";
+            ViewBag.Message = "Xyz etkinliği kayıt sayfası ";
             return View ();
         }
 
